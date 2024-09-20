8e6d7c8d0c9005011ec531e57adf34ea602620bf
LloydDub <lloydrww@gmail.com>
2024-09-20 13:51:51 -0600

    started to put vpn check in the back-end

---
+++ netlify/functions/check-vpn.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+
+// This will handle the API call to VPNAPI.io
+exports.handler = async function (event, context) {
+  // Define your VPNAPI.io token (use environment variables to hide this)
+  const vpnApiToken = process.env.VPN_API_TOKEN;
+
+  const ip = event.queryStringParameters.ip; // Get the user IP from the query parameters
+  const vpnApiUrl = `https://vpnapi.io/api/${ip}?key=${"3c641b5c07f4444dbaeba4a107d99ada"}`;
+
+  try {
+    const response = await fetch(vpnApiUrl);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error fetching VPN data" }),
+    };
+  }
+};
